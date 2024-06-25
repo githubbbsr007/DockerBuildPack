88c46d902d4d5120c95d5969148cd4c7839d453d
Asit Kumar Patel <asit-kumar.patel@eviden.com>
2024-06-25 15:51:01 +0530

    config change

--- src/main/java/ak/patel/piersquere/api/DockerBuilpackController.java
+++ src/main/java/ak/patel/piersquere/api/DockerBuilpackController.java
@@ -1,34 +1,31 @@
 package ak.patel.piersquere.api;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import ak.patel.piersquere.config.CompanyInfoConfig;
 import ak.patel.piersquere.exception.InvalidUserException;
 
 @RestController
 public class DockerBuilpackController {
 
-	@Value("${data.info}")
-	private String info;
-
-	public String getInfo() {
-		return info;
-	}
-
-	public void setInfo(String info) {
-		this.info = info;
-	}
+	@Autowired
+	private CompanyInfoConfig companyInfoConfig;
 
 	@GetMapping("/getDockerPack")
 	String getDockerBuilpack() {
 		return "DockerBuilpackConfigSuccess";
 	}
 
-	@GetMapping("/info")
-	String readInfo() {
-		return info;
+	@GetMapping("/contactInfo")
+	public String printCompanyInfo() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Company Name: ").append(companyInfoConfig.getName()).append("\n");
+		sb.append("Contact Number: ").append(companyInfoConfig.getContactNumber()).append("\n");
+		sb.append("Address: ").append(companyInfoConfig.getAddress()).append("\n");
+		return sb.toString();
 	}
 
 	@GetMapping("/createUser/{user}")
@@ -36,7 +33,6 @@ public class DockerBuilpackController {
 		if (user != null && user.equalsIgnoreCase("test")) {
 			throw new InvalidUserException("Invalid user: " + user);
 		}
-
 		return "user register success" + user;
 	}
 
